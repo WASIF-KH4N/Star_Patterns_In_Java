07b747d8e61f37a30bc9d1750079809a7c771891
Wasif Khan <m.wasif9905@gmail.com>
2024-12-14 23:29:43 +0500

    Updated Code

---
+++ Pattern_8.java
@@ -0,0 +1,13 @@
+ class Pattern_8 {
+     public static void main(String[] args) {
+         for(int i=1;i<=7;i++){
+             for(int j=7;j>=i;j--){
+                 System.out.print(" ");
+             }
+             for(int k=1;k<=i;k++){
+                 System.out.print(" *");
+             }
+             System.out.println();
+         }
+     }
+}
