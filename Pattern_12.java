5b30aab66cc168b0acdface558fd2ffdd37b7b4e
Wasif Khan <m.wasif9905@gmail.com>
2025-01-13 21:45:57 +0500

    Updated Code

---
+++ Pattern_12.java
@@ -0,0 +1,16 @@
+class Pattern_12 {
+    public static void main(String[] args) {
+        for(int i=0;i<=7;i++){
+            for(int j=0;j<=7;j++){
+                if(i>=1 && j>=1 && i<=6 && j<=6) {
+                    System.out.print(" ");
+                  }
+                    else{
+                        System.out.print("*");
+                      }
+                    }
+                  System.out.println();
+                }
+            }
+        }
+
